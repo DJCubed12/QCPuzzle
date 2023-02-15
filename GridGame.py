c03c5b5c373fac4c64c2e5ff8e9bd2dfca9e09d4
DJCubed12 <djcubed12@gmail.com>
2023-02-14 19:25:40 -0600

    Removed old functions kept only for reference

--- GridGame.py
+++ GridGame.py
@@ -17,108 +17,10 @@ COLOR_CODE = {
 }
 
 
-def Puzzle(state=None):
-    """ OURE Project prototype. """
+def main(state=None):
+    """ High level interface """
     if state is None:
         state = F.str_to_state("|00>")
-    screen = pygame.display.set_mode(SCREEN_SIZE)
-
-    second_bit = True  # Which qubit is being operated on
-    state_str = F.state_to_str(state)
-    gate_str = ''
-
-    print(f"[ Operating on 2nd qubit ]")
-
-    while True:
-        display(state, screen)
-
-        inp = input("Enter a gate (X, Y, Z, H, CNOT), Q to quit, or S to swap: \n").upper()
-        print()
-
-        if inp == '':  # No input
-            continue
-        elif inp[0] == 'Q':  # Quit
-            break
-        elif "CNOT" in inp:  # CNOT
-            if second_bit:
-                gate_str = "CNOT12"
-                gate = GATES["CNOT12"]
-            else:
-                gate_str = "CNOT21"
-                gate = GATES["CNOT21"]
-        elif inp[0] == 'S':  # Swap
-            second_bit = not second_bit
-            print(f"[ Operating on {'2nd' if second_bit else '1st'} qubit ]")
-            continue
-        else:
-            try:  # Other gate
-                gate = F.str_to_gate(inp)
-            except KeyError:  # Invalid input
-                continue
-
-            if second_bit:
-                gate = np.kron(GATES["I"], gate)
-                gate_str = 'I' + inp
-            else:
-                gate = np.kron(gate, GATES["I"])
-                gate_str = inp + 'I'
-
-        print(gate_str, state_str, '=', end=' ')
-        state = np.matmul(gate, state)
-        state_str = F.state_to_str(state)
-        print(state_str)
-
-
-def display(state, screen):
-    """Show 2 by 2 dot grid representation."""
-    FONT = pygame.font.SysFont("Britannic Bold", 40)
-    screen.fill(BLACK)
-
-    insqrt2 = 1/np.sqrt(2)
-    for i, coord in zip(state, COORDS):
-        get_box = lambda r : (*(i - r for i in coord), *(i + r for i in coord))
-
-        if isclose(i.real, 1, rel_tol=1e-15): 
-            pygame.draw.rect(screen, COLOR_CODE['1'], get_box(NORM_RADIUS))
-        elif isclose(i.real, -1, rel_tol=1e-15):
-            pygame.draw.rect(screen, COLOR_CODE['i'], get_box(NORM_RADIUS))
-        elif isclose(i.imag, 1, rel_tol=1e-15):
-            pygame.draw.rect(screen, COLOR_CODE['-1'], get_box(NORM_RADIUS))
-        elif isclose(i.imag, -1, rel_tol=1e-15):
-            pygame.draw.rect(screen, COLOR_CODE['-i'], get_box(NORM_RADIUS))
-
-        elif isclose(i.real, insqrt2, rel_tol=1e-15):
-            pygame.draw.rect(screen, COLOR_CODE['1'], get_box(NORM_RADIUS/2))
-        elif isclose(i.real, -insqrt2, rel_tol=1e-15):
-            pygame.draw.rect(screen, COLOR_CODE['i'], get_box(NORM_RADIUS/2))
-        elif isclose(i.imag, insqrt2, rel_tol=1e-15):
-            pygame.draw.rect(screen, COLOR_CODE['-1'], get_box(NORM_RADIUS/2))
-        elif isclose(i.imag, -insqrt2, rel_tol=1e-15):
-            pygame.draw.rect(screen, COLOR_CODE['-i'], get_box(NORM_RADIUS/2))
-
-        elif isclose(i.real, 1/2, rel_tol=1e-15):
-            pygame.draw.rect(screen, COLOR_CODE['1'], get_box(NORM_RADIUS/4))
-        elif isclose(i.real, -1/2, rel_tol=1e-15):
-            pygame.draw.rect(screen, COLOR_CODE['i'], get_box(NORM_RADIUS/4))
-        elif isclose(i.imag, 1/2, rel_tol=1e-15):
-            pygame.draw.rect(screen, COLOR_CODE['-1'], get_box(NORM_RADIUS/4))
-        elif isclose(i.imag, -1/2, rel_tol=1e-15):
-            pygame.draw.rect(screen, COLOR_CODE['-i'], get_box(NORM_RADIUS/4))
-
-    # screen.blit( font.render( <text>, True, <color> ), <location> )
-    screen.blit(FONT.render("|00>", True, WHITE), (30, 30))
-    screen.blit(FONT.render("|01>", True, WHITE), (230, 30))
-    screen.blit(FONT.render("|10>", True, WHITE), (30, 230))
-    screen.blit(FONT.render("|11>", True, WHITE), (230, 230))
-
-    for event in pygame.event.get():
-        pass
-
-    pygame.display.flip()
-        
-
-def main():
-    """ High level interface """
     screen, font = init()
 
     try:
@@ -140,6 +42,7 @@ def handle_events():
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # Check buttons
             pass
+
 
 def draw_states(screen, state):
     """ Takes a np.array of 4 representing the quantum state and draws to screen. Returns rects needed to be updated. """
